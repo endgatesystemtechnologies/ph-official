dd0b12f8f8a56d6567e4f33318dc9e53534a4b5f
endgatesystemtechnologies <endgate.systems@protonmail.com>
2025-07-04 22:22:34 -0600

    Update general.vue

--- frontend/src/views/settings/general.vue
+++ frontend/src/views/settings/general.vue
@@ -7,7 +7,7 @@
 
     <b-field :label="$t('settings.general.rootURL')" label-position="on-border"
       :message="$t('settings.general.rootURLHelp')">
-      <b-input v-model="data['app.root_url']" name="app.root_url" placeholder="https://listmonk.yoursite.com"
+      <b-input v-model="data['app.root_url']" name="app.root_url" placeholder="https://privatehouse.yoursite.com"
         :maxlength="300" required type="url" pattern="https?://.*" />
     </b-field>
 
@@ -15,7 +15,7 @@
       <div class="column is-6">
         <b-field :label="$t('settings.general.logoURL')" label-position="on-border"
           :message="$t('settings.general.logoURLHelp')">
-          <b-input v-model="data['app.logo_url']" name="app.logo_url" placeholder="https://listmonk.yoursite.com/logo.png"
+          <b-input v-model="data['app.logo_url']" name="app.logo_url" placeholder="https://privatehouse.yoursite.com/logo.png"
             :maxlength="300" type="url" pattern="https?://.*" />
         </b-field>
       </div>
@@ -23,7 +23,7 @@
         <b-field :label="$t('settings.general.faviconURL')" label-position="on-border"
           :message="$t('settings.general.faviconURLHelp')">
           <b-input v-model="data['app.favicon_url']" name="app.favicon_url"
-            placeholder="https://listmonk.yoursite.com/favicon.png" :maxlength="300"
+            placeholder="https://privatehouse.yoursite.com/favicon.png" :maxlength="300"
             type="url" pattern="https?://.*" />
         </b-field>
       </div>
@@ -33,7 +33,7 @@
     <b-field :label="$t('settings.general.fromEmail')" label-position="on-border"
       :message="$t('settings.general.fromEmailHelp')">
       <b-input v-model="data['app.from_email']" name="app.from_email"
-        placeholder="Listmonk <noreply@listmonk.yoursite.com>" pattern="((.+?)\s)?<(.+?)@(.+?)>" :maxlength="300" />
+        placeholder="Dakota <noreply@endgate.systems>" pattern="((.+?)\s)?<(.+?)@(.+?)>" :maxlength="300" />
     </b-field>
     <b-field :label="$t('settings.general.adminNotifEmails')" label-position="on-border"
       :message="$t('settings.general.adminNotifEmailsHelp')">
